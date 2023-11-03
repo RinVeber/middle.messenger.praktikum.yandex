5df9566a6158c51a2f514cc4286e2a387d98c2b0
Kirill Karpov <rin.veber@gmail.com>
2023-11-04 02:46:17 +0400

    fix eslint errors

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,7 @@
-export { Button } from './button/button.ts';
-export { Input } from './input/input.ts';
-export { Form } from './form/form.ts';
-export { Message } from './message/message.ts';
-export { User } from './chatListUser/chatListUser.ts';
-export { Error } from './error/error.ts';
-export { Avatar } from './avatar/avatar.ts';
-export { Info } from './info/info.ts';
+export { Button } from './button';
+export { Input } from './input';
+export { Form } from './form';
+export { Message } from './message';
+export { Error } from './error';
+export { Avatar } from './avatar';
+export { Info } from './info';
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,56 @@
-import Handlebars from 'handlebars'
-import * as Components from './components/index.ts'
-import * as Pages from './pages/index.ts'
+import Handlebars from 'handlebars';
+import * as Components from './components';
+import * as Pages from './pages';
 import './scss/index.scss';
 import avatar from './images/Union.svg';
 import search from './images/search.svg';
 
-type PagesType = 'login' | 'register' | 'chat' | 'not-found' | 'server-error' | 'profile' | 'profile-change-password' | 'profile-change-data';
+type PagesType =
+  | 'login'
+  | 'register'
+  | 'chat'
+  | 'not-found'
+  | 'server-error'
+  | 'profile'
+  | 'profile-change-password'
+  | 'profile-change-data';
 
 const pages = {
-    'login': [Pages.LoginPage],
-    'register': [Pages.RegisterPage],
-    'chat': [Pages.ChatPage, {search: search, avatar_user: avatar}],
-    'not-found' : [Pages.NotFoundPage],
-    'server-error': [Pages.ServerErrorPage],
-    'profile': [Pages.ProfilePage, {avatar: avatar}],
-    'profile-change-password': [Pages.ProfileChangePasswordPage, {avatar: avatar}],
-    'profile-change-data': [Pages.ProfileChangeDataPage, {avatar: avatar}]
-}
+  login: [Pages.LoginPage],
+  register: [Pages.RegisterPage],
+  chat: [Pages.ChatPage, { search: search, avatar_user: avatar }],
+  'not-found': [Pages.NotFoundPage],
+  'server-error': [Pages.ServerErrorPage],
+  profile: [Pages.ProfilePage, { avatar: avatar }],
+  'profile-change-password': [
+    Pages.ProfileChangePasswordPage,
+    { avatar: avatar },
+  ],
+  'profile-change-data': [Pages.ProfileChangeDataPage, { avatar: avatar }],
+};
 
-Object.entries(Components).forEach(([ name, component ]) => {
-    Handlebars.registerPartial(name, component)
-})
+Object.entries(Components).forEach(([name, component]) => {
+  Handlebars.registerPartial(name, component);
+});
 
 function navigate(page: PagesType) {
-    const [ source, context ] = pages[page]
-
-    const container = document.getElementById('app')!
-
-    container.innerHTML = Handlebars.compile(source)(context)
+  const [source, context] = pages[page];
+  const container = document.getElementById('app')!;
+  container.innerHTML = Handlebars.compile(source)(context);
 }
 
-document.addEventListener('DOMContentLoaded', () => navigate('login'))
+document.addEventListener('DOMContentLoaded', () => navigate('login'));
 
-document.addEventListener('click', e => {
-    if (!e.target) return
+document.addEventListener('click', (e) => {
+  if (!e.target) return;
 
-    const el = e.target as HTMLButtonElement
-    const page = el.getAttribute('page') as PagesType
+  const el = e.target as HTMLButtonElement;
+  const page = el.getAttribute('page') as PagesType;
 
-    if (page.length > 0) {
-        navigate(page)
+  if (page.length > 0) {
+    navigate(page);
 
-        e.preventDefault()
-        e.stopImmediatePropagation()
-    }
-})
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+});
