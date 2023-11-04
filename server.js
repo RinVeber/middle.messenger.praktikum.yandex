630d4962edae8d1f71ffb60fb5617aef697032c1
Kirill Karpov <rin.veber@gmail.com>
2023-11-04 04:14:13 +0400

    fix

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
+
+
 const app = express()
 const port = 3000
 
 app.use(express.static('dist'));
+const __dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), "..")
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "dist", "index.html"))
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
