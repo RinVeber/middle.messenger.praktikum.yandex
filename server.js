ec6a23c28d0e93dbccd0a17c9f6c629606770e64
Kirill Karpov <rin.veber@gmail.com>
2023-11-18 13:06:36 +0400

    fix

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
-import express from 'express';
-import { resolve } from 'path';
-
-const __dirname = resolve();
+import express from "express"
 
 const app = express()
 const port = 3000
 
+app.use(express.static('dist'));
 	
 app.set("view engine", "hbs");
 
-app.use(express.static(resolve(__dirname, 'dist')));
-
-app.use('/*', (_, res) => {
-  res.status(200).sendFile(resolve(__dirname, 'dist/index.html'));
-});
+app.get("/", (req, res) => {
+  res.status(200).send("main")
+})
 
 app.get("*", (req, res) => {
   res.status(404).send("404")
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port: ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
