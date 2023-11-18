28a3113060b27066898d9c507bb776d10102641e
Kirill Karpov <rin.veber@gmail.com>
2023-11-18 11:17:00 +0400

    routes deploy

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-import express from "express"
+const express = require('express');
+const path = require('path');
 
 const app = express()
 const port = 3000
 
-app.use(express.static('dist'));
 	
 app.set("view engine", "hbs");
 
-app.get("/", (req, res) => {
-  res.status(200).send("main")
-})
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.get("*", (req, res) => {
   res.status(404).send("404")
